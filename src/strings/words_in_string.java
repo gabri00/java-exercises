5214fc2c22158dc1d826b9dccdca6af5fc39da6a
gabri00 <gibby.nicchia@gmail.com>
2021-03-23 12:56:07 +0100

    strings additional exercise

--- src/strings/words_in_string.java
+++ src/strings/words_in_string.java
@@ -1,18 +1,17 @@
 package strings;
 
 import java.util.Scanner;
+import java.util.Vector;
 
 public class words_in_string {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String s;
         System.out.print("Input string: ");
-        s = input.next();
-        StringBuffer sb = new StringBuffer(s);
+        String s = input.nextLine().trim();
 
+        String[] words = s.split(" ");
+        for (String word : words) System.out.println("[" + word + "]");
 
-
-        System.out.println();
         input.close();
     }
 }
