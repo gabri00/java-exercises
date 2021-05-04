8d065426c85113621ebd878da7ff91a3795d706f
gabri00 <gibby.nicchia@gmail.com>
2021-05-04 11:38:19 +0200

    DPLL

--- src/risolProp/core/Resolution.java
+++ src/risolProp/core/Resolution.java
@@ -18,7 +18,27 @@ public class Resolution {
          *
          */
 
+        for (int i = 0; i < formula.size(); i++) {
+            Clause c1 = formula.get(i);
 
+            for (int j = 0; j < formula.size(); j++) {
+                Clause c2 = formula.get(j);
+
+                // Cerco un letterale tra c1 e c2 su cui fare la risoluzione, se non esiste lit sarà null
+                Integer lit = c1.findLiteralForResolution(c2);
+                if (lit != null) {
+                    Clause c3 = new Clause();
+                    // Risoluzione tra c1 e c2 tramite lit, crea c3
+                    c3.merge(c1, c2, lit);
+
+                    System.out.println("Ho fatto risoluzione tra " + c1 + " e " + c2 + " usando " + lit + " ottenendo la clausola" + c3);
+
+                    if (c3.isEmpty()) return false;
+
+                    formula.add(c3);
+                }
+            }
+        }
 
         return true;
     }
